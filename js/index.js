7975b30d97fc7284342a3061d7d6e394ebfe07c3
Harry <havh1_16@uni.worc.ac.uk>
2017-11-16 15:24:54 +0000

    Output Modalities
    Initial

---
+++ js/index.js
@@ -0,0 +1,37 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+function random() {
+return !Math.round(Math.random());
+}
+
+function makeBeep() {
+    //alert(random());
+    if(random == true) {
+        navigator.notification.beep(1);
+        navigator.notification.message(True);
+    }
+    else{
+        navigator.notification.beep(2);
+        navigator.notification.message(False);
+    }
+}
+
+$("#myText").text("value");
+$(document).on("pagecreate","#pageone",function(){
+  
+  	$('#taprandom').on("tap",function(){
+    	makeBeep();
+ 	});
+    
+
+	//$('#taptext').on("tap",function(){
+    	//alert("Stop tapping!");
+        //$(this).css('color', 'red');
+ 	//}); 
+
+	//$('#swipetext').on("swipeleft",function(){
+    	//$(this).css('color', 'green');
+        
+  	//});   
+
+});
